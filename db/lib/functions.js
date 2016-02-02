546f5670aa3667196744012a54e2169246973458
Maria <mmorrison1978@gmail.com>
2016-02-02 16:38:22 -0700

    adding db function

--- db/lib/functions.js
+++ db/lib/functions.js
@@ -1,7 +1,12 @@
 module.exports = [
-  $('option').mousedown(function(e) {
-    e.preventDefault();
-    $(this).prop('selected', !$(this).prop('selected'));
-    return false;
-})
+//   $('option').mousedown(function(e) {
+//     e.preventDefault();
+//     $(this).prop('selected', !$(this).prop('selected'));
+//     return false;
+// }),
+
+
+function NoControl () {
+  console.log("worked");
+}
 ]
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var categories = require('../db/lib/categories');
-var multi = require('../db/lib/functions');
+var functions = require('../db/lib/functions');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
