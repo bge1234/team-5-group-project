4d605ffb1924676f16697d11acdb9f290786b747
Ben Ebel <bge1234@gmail.com>
2016-02-02 11:21:07 -0700

    updated migrations

--- migrations/20160201112114_freebies.js
+++ migrations/20160201112114_freebies.js
@@ -2,16 +2,11 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('freebies', function(table){
     table.increments();
     table.integer('category_id');
-    table.integer('start_month');
-    table.integer('start_day');
-    table.integer('start_year');
-    table.integer('end_month');
-    table.integer('end_day');
-    table.integer('end_year');
-    table.integer('repetition_days')
+    table.string('start_date');
+    table.string('end_date');
+    table.text('text');
     table.string('name');
-    table.text('location');
-    table.text('details');
+    table.string('location');
     table.string('url');
     table.integer('creator_id');
   })
