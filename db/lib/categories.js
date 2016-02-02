c58900eddde241c512c4b08a1164f5d7cb4f7877
Maria <mmorrison1978@gmail.com>
2016-02-02 11:29:45 -0700

    adding lib files

---
+++ db/lib/categories.js
@@ -0,0 +1,14 @@
+module.exports = [
+  'outdoors',
+  'biking',
+  'hiking',
+  'culture',
+  'museums',
+  'activism',
+  'fitness',
+  'yoga',
+  'running',
+  'music',
+  'family',
+  'goods'
+]
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var categories = require('../db/lib/categories')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
