8c71c8951e9572d7de7aee03f34b52735faeac4a
Kinsey Ashby <kinseyzaire@gmail.com>
2016-02-03 10:27:55 -0700

    small splash page changes in routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,33 @@
 var express = require('express');
+var knex = require('../db/knex.js');
 var router = express.Router();
 var request = require('request');
-var categories = require('../db/lib/categories')
+
+function Users() {
+   return knex('megausers');
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'FreeDenver.co' });
   res.redirect('/freebies');
+});
+
+router.get('/info/corporate-partner', function(req, res, next) {
+  res.render('info/corporate-partner-info');
+});
+
+// post from splash page
+router.post('/splash', function(req, res, next) {
+  var categories = req.body.categories
+  res.redierct('freebies/index',{category: category});
+});
+
+// show all megausers
+router.get('/megausers', function(req, res, next) {
+  Users().select().then(function(results) {
+    res.render('megausers/index', {users: results});
+  });
 });
 
 // about us page
@@ -19,11 +40,12 @@ router.get('/info/contact', function(req, res, next) {
   res.render('info/contact');
 });
 
-// contact us page
+// map page
 router.get('/map', function(req, res, next) {
   res.render('map', {lat: 39.757785, lng: -105.007142});
 });
 
+// map page
 router.get('/mapsearch', function(req, res, next) {
   request("https://maps.googleapis.com/maps/api/geocode/json?address=" + req.param('location').split(' ').join('+'), function (error, response, body) {
     if (!error && response.statusCode == 200) {
