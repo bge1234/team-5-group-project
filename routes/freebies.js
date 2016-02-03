cf7c6d67712cc6baad1aae116c5fb226f906296d
Ben Ebel <bge1234@gmail.com>
2016-02-02 20:00:35 -0700

    finished times and addresses

--- routes/freebies.js
+++ routes/freebies.js
@@ -20,13 +20,53 @@ function admin(){
  return knex('admin');
 };
 
+function getMonth(yearString) {
+  if(yearString === "Jan")
+    return 1;
+  else if(yearString === "Feb")
+    return 2;
+  else if(yearString === "Mar")
+    return 3;
+  else if(yearString === "Apr")
+    return 4;
+  else if(yearString === "May")
+    return 5;
+  else if(yearString === "Jun")
+    return 6;
+  else if(yearString === "Jul")
+    return 7;
+  else if(yearString === "Aug")
+    return 8;
+  else if(yearString === "Sep")
+    return 9;
+  else if(yearString === "Oct")
+    return 10;
+  else if(yearString === "Nov")
+    return 11;
+  else
+    return 12;
+}
+
 // get activites and maps page
 router.get('/', function(req, res, next) {
   freebies().select().then(function(results) {
     unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + apiKey)
      .end(function(response) {
+
        var meetups = response.body.results;
-        res.render('freebies/index', {freebies: results, events: meetups});
+       var dates = [];
+
+        for (var i = 0; i < meetups.length; i++) {
+          date = new Date(meetups[i]["time"]);
+          var dateString = date.toString().split(' ');
+          var month = getMonth(dateString[1]).toString();
+          var day = dateString[2];
+          var year = dateString[3];
+          var time = dateString[4];
+          dates.push(month + '/' + day + '/' + year + ' ' + time);
+        }
+
+        res.render('freebies/index', {freebies: results, events: meetups, dates: dates});
     })
   })
 });
