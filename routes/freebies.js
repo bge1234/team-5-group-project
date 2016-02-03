500cb257d6d66d51ae43c4df297a4fbf0bd81042
Kinsey Ashby <kinseyzaire@gmail.com>
2016-02-03 13:22:38 -0700

    styled category form

--- routes/freebies.js
+++ routes/freebies.js
@@ -17,15 +17,28 @@ function Categories(){
 };
 
 // home + freebies all
-router.get('/freebies', function(req, res, next) {
-  Freebies().select().then(function(results) {
-    unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + '3f7d255365182d12465e396b6267182e')
-     .end(function(response) {
-       var meetups = response.body.results;
-        res.render('freebies/index', {freebies: results, events: meetups, lat: 39.757785, lng: -105.007142});
+router.get('/freebies/:categoryid', function(req, res, next) {
+  Categories().select().then(function(categoryresults) {
+    Freebies().where('category_id',req.params.categoryid).then(function(results) {
+      unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + '3f7d255365182d12465e396b6267182e')
+       .end(function(response) {
+          var meetups = response.body.results;
+          res.render('freebies/index', {freebies: results, events: meetups, lat: 39.757785, lng: -105.007142, categories: categoryresults});
+        });
     });
   });
 });
+
+// home + freebies all
+// router.get('/freebies', function(req, res, next) {
+//   Freebies().select().then(function(results) {
+//     unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + '3f7d255365182d12465e396b6267182e')
+//      .end(function(response) {
+//        var meetups = response.body.results;
+//         res.render('freebies/index', {freebies: results, events: meetups, lat: 39.757785, lng: -105.007142});
+//     });
+//   });
+// });
 
 
 
