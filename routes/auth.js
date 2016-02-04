160badb7d79c5e6e57cd94e10d001944b341bb11
Ben Ebel <bge1234@gmail.com>
2016-02-04 15:43:23 -0700

    save

--- routes/auth.js
+++ routes/auth.js
@@ -14,16 +14,25 @@ router.get('/signin', function(req, res, next) {
 
 router.post('/signin', function(req, res, next) {
   Users().where('username', req.body.username).first().then(function(result){
-    bcrypt.compare(req.body.password, result.password, function(err, res2) {
-      if(res2 === true) {
-        res.cookie("current_user", req.body.username);
-        res.redirect('/' + result.username + '/freebies');
+    var testObj = {
+      username: req.body.username
+    };
 
-      }
-      else {
-        res.render('signin/signin', {error: "Incorrect username or password"});
-      }
-    });
+    if(!validate.duplicateUser(testObj, result, ["username"])) {
+      res.render('signin/signin', {error: "Username does not exist"});
+    }
+    else {
+      bcrypt.compare(req.body.password, result.password, function(err, res2) {
+        if(res2 === true) {
+          res.cookie("current_user", req.body.username);
+          res.redirect('/' + result.username + '/freebies');
+
+        }
+        else {
+          res.render('signin/signin', {error: "Incorrect username or password"});
+        }
+      });
+    }
   });
 });
 
