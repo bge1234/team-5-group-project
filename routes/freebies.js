1d32aebc1a520d9fee2fc15264d964d3e7999868
Ben Ebel <bge1234@gmail.com>
2016-02-03 18:13:45 -0700

    fixed issue of category dropdown not showing on /freebies

--- routes/freebies.js
+++ routes/freebies.js
@@ -25,17 +25,19 @@ function Admin(){
 
 // get activites and maps page
 router.get('/freebies', function(req, res, next) {
-  Freebies().select().then(function(results) {
-    unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + apiKey)
-     .end(function(response) {
+  Categories().select().then(function(categoryresults) {
+    Freebies().select().then(function(results) {
+      unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + apiKey)
+       .end(function(response) {
 
-      var meetups = response.body.results;
-      var startDates = dates.starts(meetups);
-      var endDates = dates.ends(meetups);
+        var meetups = response.body.results;
+        var startDates = dates.starts(meetups);
+        var endDates = dates.ends(meetups);
 
-      res.render('freebies/index', {freebies: results, events: meetups, startDates: startDates, endDates: endDates});
+        res.render('freebies/index', {freebies: results, events: meetups, startDates: startDates, endDates: endDates, categories: categoryresults});
+      })
     })
-  })
+  });
 });
 
 // show add freebies page
