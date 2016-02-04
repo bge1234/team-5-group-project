513f997d6387851c52d032463afbbcdb1f82a84d
Ben Ebel <bge1234@gmail.com>
2016-02-04 13:53:00 -0700

    swtiched category urls to use name instead of ID

--- routes/freebies.js
+++ routes/freebies.js
@@ -41,17 +41,19 @@ router.get('/freebies', function(req, res, next) {
 });
 
 // freebies sorted by category
-router.get('/freebies/:categoryid', function(req, res, next) {
-  Categories().select().then(function(categoryresults) {
-    Freebies().where('category_id',req.params.categoryid).then(function(results) {
-      unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + '3f7d255365182d12465e396b6267182e')
-       .end(function(response) {
+router.get('/freebies/:category', function(req, res, next) {
+  Categories().select().then(function(allcategories) {
+    Categories().where('name', req.params.category).first().then(function(categoryresult) {
+      Freebies().where('category_id',categoryresult.id).then(function(results) {
+        unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + '3f7d255365182d12465e396b6267182e')
+         .end(function(response) {
 
-         var meetups = response.body.results;
-         var startDates = dates.starts(meetups);
-         var endDates = dates.ends(meetups);
+           var meetups = response.body.results;
+           var startDates = dates.starts(meetups);
+           var endDates = dates.ends(meetups);
 
-         res.render('freebies/index', {freebies: results, events: meetups, lat: 39.757785, lng: -105.007142, categories: categoryresults, startDates: startDates, endDates: endDates, catID: parseInt(req.params.categoryid)});
+           res.render('freebies/index', {freebies: results, events: meetups, lat: 39.757785, lng: -105.007142, categories: allcategories, startDates: startDates, endDates: endDates, catname: req.params.category});
+        });
       });
     });
   });
