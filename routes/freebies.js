dea86ca0e727a9e1ba51fcdefcfd9e7720a6297f
Ben Ebel <bge1234@gmail.com>
2016-02-03 17:12:58 -0700

    stuff

--- routes/freebies.js
+++ routes/freebies.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var knex = require('../db/knex');
 var unirest = require('unirest');
-<<<<<<< HEAD
 var dates = require('../lib/dates');
-=======
 var validate = require('../lib/validations');
 var apiKey = process.env.MEETUP_API_KEY;
 var router = express.Router();
 var request = require('request');
->>>>>>> origin
 
 function Freebies(){
  return knex('freebies');
@@ -22,38 +19,9 @@ function Categories(){
  return knex('categories');
 };
 
-<<<<<<< HEAD
 function Admin(){
  return knex('admin');
 };
-=======
-function getMonth(yearString) {
-  if(yearString === "Jan")
-    return 1;
-  else if(yearString === "Feb")
-    return 2;
-  else if(yearString === "Mar")
-    return 3;
-  else if(yearString === "Apr")
-    return 4;
-  else if(yearString === "May")
-    return 5;
-  else if(yearString === "Jun")
-    return 6;
-  else if(yearString === "Jul")
-    return 7;
-  else if(yearString === "Aug")
-    return 8;
-  else if(yearString === "Sep")
-    return 9;
-  else if(yearString === "Oct")
-    return 10;
-  else if(yearString === "Nov")
-    return 11;
-  else
-    return 12;
-}
->>>>>>> origin
 
 // get activites and maps page
 router.get('/freebies', function(req, res, next) {
@@ -61,7 +29,6 @@ router.get('/freebies', function(req, res, next) {
     unirest.get('https://api.meetup.com/2/open_events?&sign=true&photo-host=public&country=us&city=denver&state=co&text=free&category=1,18,4,5,6,8,9,11,14,15,17,20,21&radius=15&status=upcoming&key=' + apiKey)
      .end(function(response) {
 
-<<<<<<< HEAD
       var meetups = response.body.results;
       var startDates = dates.starts(meetups);
       var endDates = dates.ends(meetups);
@@ -74,38 +41,6 @@ router.get('/freebies', function(req, res, next) {
 // show add freebies page
 router.get('/new', function(req, res, next) {
   res.render('freebies/new');
-=======
-       var meetups = response.body.results;
-       var startDates = [];
-       var endDates = [];
-
-      for (var i = 0; i < meetups.length; i++) {
-        date = new Date(meetups[i]["time"]);
-        var dateString = date.toString().split(' ');
-        var month = getMonth(dateString[1]).toString();
-        var day = dateString[2];
-        var year = dateString[3];
-        var time = dateString[4];
-        startDates.push(month + '/' + day + '/' + year + ' ' + time);
-      }
-
-      for (var i = 0; i < meetups.length; i++) {
-        if(meetups[i]["duration"]) {
-          date = new Date(meetups[i]["time"] + meetups[i]["duration"]);
-          var dateString = date.toString().split(' ');
-          var month = getMonth(dateString[1]).toString();
-          var day = dateString[2];
-          var year = dateString[3];
-          var time = dateString[4];
-          endDates.push(month + '/' + day + '/' + year + ' ' + time);
-        }
-        else
-          endDates.push("Not provided");
-      }
-        res.render('freebies/index', {freebies: results, events: meetups, startDates: startDates, endDates: endDates});
-    });
-  });
->>>>>>> origin
 });
 
 // add freebies
