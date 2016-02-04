f1be10ecaf54a576cbb36a5a90753fd4d404d8f0
Mikel <mikelbrierly@gmail.com>
2016-02-04 11:02:21 -0700

    filter is working

--- public/javascripts/filter.js
+++ public/javascripts/filter.js
@@ -1,25 +1,10 @@
 $(document).ready(function() {
-$('.options').hide();
-$('.fa').click(function(){
-  $(".options").animate({
-  height: "toggle"
-    }, 100, function() {
-  });
-})
-  $('#cbox1').change(function() {
-    this.checked ? console.log('Goods is checked!') : console.log('Goods is un-checked!');
-  })
-  $('#cbox2').change(function() {
-    this.checked ? console.log('Music is checked!') : console.log('Music is un-checked!');
-  })
-  $('#cbox3').change(function() {
-    this.checked ? console.log('Art & Culture is checked!') : console.log('Art & Culture is un-checked!');
-  })
-  $('#cbox4').change(function() {
-    this.checked ? console.log('Outdoors is checked!') : console.log('Outdoors is un-checked!');
-  })
-  $('#cbox5').change(function() {
-    this.checked ? console.log('Family is checked!') : console.log('Family is un-checked!');
+$('.filterRow').hide();
+$('.filterIcon').click(function(){
+  $(".filterRow").animate({
+    height: "toggle"
+      }, 100, function() {
+    });
   })
 })
 function sticky_relocate() {
@@ -31,7 +16,8 @@ function sticky_relocate() {
         $('#sticky').removeClass('stick');
     }
 }
+
 $(function () {
-    $(window).scroll(sticky_relocate);
-    sticky_relocate();
+  $(window).scroll(sticky_relocate);
+  sticky_relocate();
 });
